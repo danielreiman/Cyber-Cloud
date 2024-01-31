7e2c710f2925a29ef7ade338c1b838fab5f00f0f
ChickenRei <descant.draft0s@icloud.com>
2024-01-31 18:54:10 +0200

    Inital Commit

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyBlfWBWbLPOYNJMTgdshpW5SOESpPZ7btM",
-  authDomain: "cyber-cloud-6caec.firebaseapp.com",
-  projectId: "cyber-cloud-6caec",
-  storageBucket: "cyber-cloud-6caec.appspot.com",
-  messagingSenderId: "700790763944",
-  appId: "1:700790763944:web:51f3642ad467eb0789ed9d"
-};
+var _0x401f24=_0x13ad;function _0x13ad(_0x35c748,_0x1afe65){var _0x29e451=_0x29e4();return _0x13ad=function(_0x13ad4b,_0x1a9411){_0x13ad4b=_0x13ad4b-0x188;var _0x27bd7b=_0x29e451[_0x13ad4b];return _0x27bd7b;},_0x13ad(_0x35c748,_0x1afe65);}function _0x29e4(){var _0x1839ae=['143372YLWpOK','cyber-cloud-6caec.appspot.com','15ogpdUk','1:700790763944:web:51f3642ad467eb0789ed9d','467768VrwgoC','21920gEqwOp','2298605KZflqx','6YgnTNA','399722FKODTM','1896OiObII','2493bekAwD','cyber-cloud-6caec.firebaseapp.com','cyber-cloud-6caec','3241JkQXVu','AIzaSyBlfWBWbLPOYNJMTgdshpW5SOESpPZ7btM','8856738jVorPf','700790763944'];_0x29e4=function(){return _0x1839ae;};return _0x29e4();}(function(_0x2fa6b8,_0x518f4f){var _0x63c0ba=_0x13ad,_0x50d80f=_0x2fa6b8();while(!![]){try{var _0x461444=parseInt(_0x63c0ba(0x189))/0x1+parseInt(_0x63c0ba(0x196))/0x2+parseInt(_0x63c0ba(0x194))/0x3*(parseInt(_0x63c0ba(0x192))/0x4)+parseInt(_0x63c0ba(0x198))/0x5*(-parseInt(_0x63c0ba(0x188))/0x6)+-parseInt(_0x63c0ba(0x18e))/0x7*(parseInt(_0x63c0ba(0x18a))/0x8)+-parseInt(_0x63c0ba(0x18b))/0x9*(parseInt(_0x63c0ba(0x197))/0xa)+parseInt(_0x63c0ba(0x190))/0xb;if(_0x461444===_0x518f4f)break;else _0x50d80f['push'](_0x50d80f['shift']());}catch(_0x48fd6c){_0x50d80f['push'](_0x50d80f['shift']());}}}(_0x29e4,0x6bbff));var firebaseConfig={'apiKey':_0x401f24(0x18f),'authDomain':_0x401f24(0x18c),'projectId':_0x401f24(0x18d),'storageBucket':_0x401f24(0x193),'messagingSenderId':_0x401f24(0x191),'appId':_0x401f24(0x195)};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const storage = firebase.storage();
